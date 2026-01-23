109ea291578da368810d2a1f40e878ada48d746c
Mobeen <141554893+mobeen-asif@users.noreply.github.com>
2026-01-23 21:11:21 +0500

    Completed.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
   return gulp
     .src('src/sass/**/*.scss')  
     .pipe(sass().on('error', sass.logError))
+    .pipe(purgecss({
+      content: ['*.html'],
+    }))
     .pipe(gulp.dest('dist/css'));
 }
 
 function watchTask() {
-  gulp.watch('src/sass/**/*.scss', buildStyles); 
+  gulp.watch(['src/sass/**/*.scss', '*.html'], buildStyles); 
 }
 
 exports.default = gulp.series(buildStyles, watchTask);
