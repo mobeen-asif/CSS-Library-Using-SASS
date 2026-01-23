c8892e06de2f7878aacb5f8a1147890cc0b3b71f
Mobeen <141554893+mobeen-asif@users.noreply.github.com>
2026-01-23 21:20:27 +0500

    Completed.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const purgecss = require('gulp-purgecss');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
-function buildStyles() {
+function build() {
   return gulp
-    .src('src/sass/**/*.scss')  
-    .pipe(sass().on('error', sass.logError))
-    .pipe(purgecss({
-      content: ['*.html'],
-    }))
-    .pipe(gulp.dest('dist/css'));
+    .src("src/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      purgecss({
+        content: ["*.html"],
+      }),
+    )
+    .pipe(gulp.dest("dist/css"));
 }
 
 function watchTask() {
-  gulp.watch(['src/sass/**/*.scss', '*.html'], buildStyles); 
+  gulp.watch(["src/sass/**/*.scss", "*.html"], build);
 }
 
-exports.default = gulp.series(buildStyles, watchTask);
+exports.default = gulp.series(build, watchTask);
